b6f35a1bf40daca71e8abbe0dc4bff6195b6a807
juansinwifi <ing.sebastianotero@gmail.com>
2020-03-01 03:13:12 +0000

    Cambio de path scripts

--- jenkins/build/maven.sh
+++ jenkins/build/maven.sh
@@ -4,5 +4,5 @@ echo "****************"
 echo "* Building jar!*"
 echo "****************"
 
-PROJ_PATH=/home/masteryoda/pipeline
+PROJ_PATH=/var/jenkins_home/workspace/pipeline-docker-maven
 docker run --rm -v /root/.m2:/root/.m2 -v $PROJ_PATH/java-app:/app -w /app maven:3-alpine mvn -B -DskipTests clean package
