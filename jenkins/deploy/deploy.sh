6a0caf2f0ee4f177438cd7d67310abf8037aadc2
juansinwifi <ing.sebastianotero@gmail.com>
2020-03-01 02:49:16 +0000

    Change keys path

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -6,6 +6,6 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 #Transferimos el archivo 
-scp -P 22000 -i ~/mykeys/labaws.pem /tmp/.auth ubuntu@api.alivio.b612.cloud:/tmp/.auth
-scp -P 22000 -i ~/mykeys/labaws.pem ./jenkins/deploy/publish.sh ubuntu@api.alivio.b612.cloud:/tmp/publish.sh
-ssh -i ~/mykeys/labaws.pem ubuntu@api.alivio.b612.cloud -p 22000 /tmp/publish.sh
+scp -P 22000 -i /opt/mykeys/labaws.pem /tmp/.auth ubuntu@api.alivio.b612.cloud:/tmp/.auth
+scp -P 22000 -i /opt/mykeys/labaws.pem ./jenkins/deploy/publish.sh ubuntu@api.alivio.b612.cloud:/tmp/publish.sh
+ssh -i /opt/mykeys/labaws.pem ubuntu@api.alivio.b612.cloud -p 22000 /tmp/publish.sh
